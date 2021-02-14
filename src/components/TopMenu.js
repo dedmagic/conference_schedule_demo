6e084eff47ad7bbc17304fcd13f65bc4a6a10256
dedmagic <dedmagic@gmail.com>
2021-02-14 17:48:00 +0500

    top menu begin

---
+++ src/components/TopMenu.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class TopMenu extends React.Component {
+    render() {
+        return (
+            <div class="navbar">
+                <ul class="navbar-nav">
+                    <li>First</li>
+                    <li>Second</li>
+                </ul>
+            </div>
+        );
+    }
+}
+
+export default TopMenu;
