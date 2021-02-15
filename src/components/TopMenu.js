11b2c6743075a4307430db8643db8865264e9de4
dedmagic <dedmagic@gmail.com>
2021-02-15 10:07:06 +0500

    make horizontal menu

--- src/components/TopMenu.js
+++ src/components/TopMenu.js
@@ -3,12 +3,25 @@ import React from 'react';
 class TopMenu extends React.Component {
     render() {
         return (
-            <div class="navbar">
-                <ul class="navbar-nav">
-                    <li>First</li>
-                    <li>Second</li>
-                </ul>
-            </div>
+            <nav class="navbar navbar-expand-lg navbar-light">
+                <div class="container-fluid">
+                    <a class="navbar-brand" href="#">Расписание конференции (демо)</a>
+
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav">
+                            <li class="nav-item active">
+                                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Link</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link disabled" href="#">Disabled</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
         );
     }
 }
