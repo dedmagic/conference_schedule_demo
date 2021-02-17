34278e2af5c6bd864b24eedd0ebb2a7f9df12bbb
dedmagic <dedmagic@gmail.com>
2021-02-17 10:09:43 +0500

    add routing

--- src/components/App.js
+++ src/components/App.js
@@ -2,19 +2,25 @@ import '../css/App.css';
 import AboutConference from './AboutConference';
 import Speakears from './Speakers';
 import TopMenu from './TopMenu';
+import { Link, Route, BrowserRouter } from 'react-router-dom';
 
 function App() {
-  return (
-    <div class="container">
-        <div class="row">
-            <TopMenu />
+    return (
+        <div class="container">
+            <div class="row">
+                <TopMenu />
+            </div>
+            <div class="row">
+                <BrowserRouter>
+                    <Link to="/"></Link>
+                    <Route exact path="/" component={AboutConference}></Route>
+
+                    <Link to="/speakers"></Link>
+                    <Route exact path="/speakers" component={Speakears}></Route>
+                </BrowserRouter>
+            </div>
         </div>
-        <div class="row">
-            {/* <AboutConference /> */}
-            <Speakears />
-        </div>
-    </div>    
-  );
+    );
 }
 
 export default App;
--- src/components/TopMenu.js
+++ src/components/TopMenu.js
@@ -10,13 +10,13 @@ class TopMenu extends React.Component {
                     <div class="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul class="navbar-nav">
                             <li class="nav-item">
-                                <a class="nav-link" href="#">О конференции<span class="sr-only">(current)</span></a>
+                                <a class="nav-link" href="/">О конференции<span class="sr-only">(current)</span></a>
                             </li>
                             <li class="nav-item">
                                 <a class="nav-link" href="#">Программа</a>
                             </li>
                             <li class="nav-item active">
-                                <a class="nav-link" href="#">Спикеры</a>
+                                <a class="nav-link" href="/speakers">Спикеры</a>
                             </li>
                         </ul>
                     </div>
